45a50e07ec046591890350ed5904fd2859c8e5d0
matecon <matecon.test@protonmail.com>
2021-10-27 14:38:48 -0300

    create header

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Tinder clone using MERN.
 
-This is a tutorial i'm following from [Clever Programmer](https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ).
-
-The video is - [Learn the MERN Stack - Full Tutorial for Beginners (MongoDB, Express, React, NodeJS) in 12Hrs (2021)](https://www.youtube.com/watch?v=ktjafK4SgWM).
+I am following a [tutorial](https://www.youtube.com/watch?v=ktjafK4SgWM) from [Clever Programmer](https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ).
 
 ## [Github Repo](https://github.com/MateConTest/tinder-clone)
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./Header.css";
+import IconButton from '@material-ui/core/IconButton';
+import PersonIcon from '@material-ui/icons/Person'
+import ForumIcon from '@material-ui/icons/Forum';
+import TinderIcon from './assets/tinder-icon.png';
 
 const Header = () => {
     return (
         <div className="header">
-            <h2>I am header</h2>
+            <IconButton>
+                <PersonIcon
+                    fontSize="large"
+                    className="header__icon"
+                />
+            </IconButton>
+
+            <img
+                className="header__logo"
+                src={TinderIcon}
+                alt="logo"
+            />
+
+            <IconButton>
+                <ForumIcon
+                    fontSize="large"
+                    className="header__icon"
+                />
+            </IconButton>
         </div>
     )
 }
