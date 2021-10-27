790034351a0fbc39b125159a9f373adb8a55de9f
matecon <matecon.test@protonmail.com>
2021-10-27 19:53:00 -0300

    setup env

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // App config
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+const connection_url = process.env.DB_CONNECTION;
 
 // Middlewares
 
