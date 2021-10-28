9d254a7b833fc4e9ea0eb8405057aa2bc75b91d5
matecon <matecon.test@protonmail.com>
2021-10-28 10:20:44 -0300

    connect backend to db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import Cards from './dbCards.js';
 
 // App config
 dotenv.config();
@@ -9,12 +11,37 @@ const port = process.env.PORT || 8080;
 const connection_url = process.env.DB_CONNECTION;
 
 // Middlewares
+app.use(express.json());
+app.use(cors());
 
 // DB config
+mongoose.connect(connection_url);
 
 // API Endpoints
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
+});
+
+app.post('/tinder/card', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
 });
 
 // Listener
